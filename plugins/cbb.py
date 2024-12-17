dbcae543407c300b1b84315edf956964cc39b773
Rahulkumartv344 <rahulkumatv9999@gmail.com>
2024-12-17 20:35:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "More":
+    if data == "about":
         await query.message.edit_text(
             text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Jack</a>\n○ Channel : @animeworldhero○ Support Group : @animeworldhero</b>",
             disable_web_page_preview = True,
@@ -20,7 +20,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 ]
             )
         )
-    elif data == "Close":
+    elif data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()

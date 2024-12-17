682d879d8522eb8100e9c128b37f17965a9f6897
Rahulkumartv344 <rahulkumatv9999@gmail.com>
2024-12-17 18:32:27 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "More":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Jack</a>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @@animeworldhero○ Support Group : @@animeworldhero</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Jack</a>\n○ Channel : @animeworldhero○ Support Group : @animeworldhero</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
